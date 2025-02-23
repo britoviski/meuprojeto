0562810782444221c2d6d9919ffc528fb367ac17
britoviski <98991610+britoviski@users.noreply.github.com>
2025-02-23 18:15:45 -0300

    selecao por id

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -12,8 +12,10 @@ class ProdutoController extends Controller
      */
     public function index()
     {
-        $produtos = Produto::orderBy('nome', 'ASC')->get();
-        dd($produtos);
+        //$produtos = Produto::orderBy('nome', 'ASC')->get();
+        //dd($produtos);
+        $produto = Produto::find(1);
+        dd($produto);
     }
 
     /**
